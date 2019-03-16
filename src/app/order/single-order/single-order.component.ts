50498455c4ab7826c4da9b05052d13e1dd33112c
Svein Are Danielsen <sveindani@gmail.com>
2019-03-16 20:56:27 +0100

    Added functions for retrieving single order

--- src/app/order/single-order/single-order.component.ts
+++ src/app/order/single-order/single-order.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../order.service';
+import { ActivatedRoute } from '@angular/router';
+import { Order } from '../order';
+import { CustomerService } from 'src/app/customer/customer.service';
 
 @Component({
   selector: 'app-single-order',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./single-order.component.scss']
 })
 export class SingleOrderComponent implements OnInit {
+  constructor(
+    private orderService: OrderService, 
+    private customerService: CustomerService, 
+    private route: ActivatedRoute,
+    ) {}
 
-  constructor() { }
+  order: Order;
 
   ngOnInit() {
+    let id = this.route.snapshot.params.id
+    this.getOrder(id);
   }
 
+  getOrder(id) {
+    this.orderService.getOrder(id).subscribe((order) => {
+      this.order = order;
+    }, (err) => { console.error(err); }, () => {
+      this.getCustomer(this.order.customerId);
+    });
+  }
+
+  getCustomer(id) {
+    this.customerService.getCustomer(id).subscribe((customer) => {
+      this.order.customer = customer;
+    }, (err) => { console.error(err); }, () => {});
+  }
 }
