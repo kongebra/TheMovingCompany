3154f4205b71d62cb938f12aa1295856bf77178c
Svein Are Danielsen <sveindani@gmail.com>
2019-03-16 14:16:26 +0100

    Added som function to all component

--- src/app/order/all/all.component.ts
+++ src/app/order/all/all.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../order.service';
+import { Order } from '../order';
 
 @Component({
-  selector: 'app-all',
-  templateUrl: './all.component.html',
-  styleUrls: ['./all.component.scss']
+	selector: 'app-all',
+	templateUrl: './all.component.html',
+	styleUrls: ['./all.component.scss']
 })
 export class AllComponent implements OnInit {
+	constructor(private service: OrderService) { }
 
-  constructor() { }
+	orders: Order[] = [];
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.getOrders();
+	}
+
+	getOrders() {
+		this.service.getOrders().subscribe(orders => this.orders = orders);
+	}
 
 }
