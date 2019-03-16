36b1b27d35bfd1d6260dd81f3754c3e35b211600
Svein Are Danielsen <sveindani@gmail.com>
2019-03-16 20:56:00 +0100

    Update delete order function Update redirect after subscrition is over

--- src/app/order/delete-order/delete-order.component.ts
+++ src/app/order/delete-order/delete-order.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../order.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-delete-order',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./delete-order.component.scss']
 })
 export class DeleteOrderComponent implements OnInit {
-
-  constructor() { }
+  constructor(private service: OrderService, private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit() {
+    let id = this.route.snapshot.paramMap.get('id');
+    this.deleteOrder(parseInt(id));
+  }
+
+  deleteOrder(id: number) {
+    this.service.deleteOrder(id).subscribe(() => {
+
+    }, err => console.error(err), () => {
+      this.router.navigate(['/']);
+    });
   }
 
 }
