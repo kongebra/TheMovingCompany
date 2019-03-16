f5bc9b35005d47e1dd6320023c2430567f5166b4
Svein Are Danielsen <sveindani@gmail.com>
2019-03-16 22:10:03 +0100

    Added count of orders per customer

--- src/app/customer/all-customers/all-customers.component.ts
+++ src/app/customer/all-customers/all-customers.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CustomerService } from '../customer.service';
 import { Customer } from '../customer';
+import { OrderService } from 'src/app/order/order.service';
+import { Order } from 'src/app/order/order';
 
 @Component({
 	selector: 'app-all-customers',
@@ -8,16 +10,33 @@ import { Customer } from '../customer';
 	styleUrls: ['./all-customers.component.scss']
 })
 export class AllCustomersComponent implements OnInit {
-	constructor(private customerService: CustomerService) { }
+	constructor(
+		private customerService: CustomerService,
+		private orderService: OrderService,
+	) { }
 
 	customers: Customer[] = [];
 
 	ngOnInit() {
 		this.getCustomers();
+		this.getOrders();
 	}
 
 	getCustomers() {
 		this.customerService.getCustomers().subscribe(customers => this.customers = customers);
+	}
+
+	getOrders() {
+		let orders: Order[];
+
+		this.orderService.getOrders().subscribe(o => orders = o, err => console.error(err), () => {
+			this.customers.forEach(customer => {
+				customer.orders = orders;
+				customer.orders.filter(e => {
+					return e.customerId == customer.id;
+				})
+			});
+		});
 	}
 
 }
