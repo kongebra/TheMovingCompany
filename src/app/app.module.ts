8b0264aa9238a584c7e74d6c00369db2ad70e3df
Svein Are Danielsen <sveindani@gmail.com>
2019-03-16 16:47:47 +0100

    Added new components Added already existing, with new more elaborate names

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,16 @@ import { AllComponent } from './order/all/all.component';
 import { SingleComponent } from './order/single/single.component';
 import { AppRoutingModule } from './app-routing.module';
 import { EnumToArrayPipe } from './order/enum-to-array.pipe';
+import { AllCustomersComponent } from './customer/all-customers/all-customers.component';
+import { AllOrdersComponent } from './order/all-orders/all-orders.component';
+import { SingleOrderComponent } from './order/single-order/single-order.component';
+import { NewOrderComponent } from './order/new-order/new-order.component';
+import { UpdateOrderComponent } from './order/update-order/update-order.component';
+import { SingleCustomerComponent } from './customer/single-customer/single-customer.component';
+import { NewCustomerComponent } from './customer/new-customer/new-customer.component';
+import { UpdateCustomerComponent } from './customer/update-customer/update-customer.component';
+import { DeleteCustomerComponent } from './customer/delete-customer/delete-customer.component';
+import { DeleteOrderComponent } from './order/delete-order/delete-order.component';
 
 @NgModule({
   declarations: [
@@ -20,6 +30,16 @@ import { EnumToArrayPipe } from './order/enum-to-array.pipe';
     AllComponent,
     SingleComponent,
     EnumToArrayPipe,
+    AllCustomersComponent,
+    AllOrdersComponent,
+    SingleOrderComponent,
+    NewOrderComponent,
+    UpdateOrderComponent,
+    SingleCustomerComponent,
+    NewCustomerComponent,
+    UpdateCustomerComponent,
+    DeleteCustomerComponent,
+    DeleteOrderComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/customer/single-customer/single-customer.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-single-customer',
+  templateUrl: './single-customer.component.html',
+  styleUrls: ['./single-customer.component.scss']
+})
+export class SingleCustomerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
