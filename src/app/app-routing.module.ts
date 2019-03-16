f1f9a013946a9b664b27917b179f520fc661e132
Svein Are Danielsen <sveindani@gmail.com>
2019-03-16 20:52:48 +0100

    Added more routes and tidy up the file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,34 +4,31 @@ import { AllOrdersComponent } from './order/all-orders/all-orders.component';
 import { NewOrderComponent } from './order/new-order/new-order.component';
 import { AllCustomersComponent } from './customer/all-customers/all-customers.component';
 import { SingleOrderComponent } from './order/single-order/single-order.component';
+import { NewCustomerComponent } from './customer/new-customer/new-customer.component';
+import { SingleCustomerComponent } from './customer/single-customer/single-customer.component';
+import { DeleteOrderComponent } from './order/delete-order/delete-order.component';
+import { UpdateOrderComponent } from './order/update-order/update-order.component';
+import { UpdateCustomerComponent } from './customer/update-customer/update-customer.component';
+import { DeleteCustomerComponent } from './customer/delete-customer/delete-customer.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/orders',
-    pathMatch: 'full'
-  },
-  {
-    path: 'order/:id',
-    component: SingleOrderComponent
-  },
-  {
-    path: 'orders',
-    component: AllOrdersComponent
-  },
-  {
-    path: 'orders/new',
-    component: NewOrderComponent
-  },
+	{ path: '', redirectTo: '/orders', pathMatch: 'full' },
 
-  {
-    path: 'customers',
-    component: AllCustomersComponent
-  }
+	{ path: 'orders', component: AllOrdersComponent },
+	{ path: 'orders/new', component: NewOrderComponent },
+	{ path: 'orders/view/:id', component: SingleOrderComponent },
+	{ path: 'orders/update/:id', component: UpdateOrderComponent },
+	{ path: 'orders/delete/:id', component: DeleteOrderComponent },
+
+	{ path: 'customers', component: AllCustomersComponent },
+	{ path: 'customers/new', component: NewCustomerComponent },
+	{ path: 'customers/view/:id', component: SingleCustomerComponent	},
+	{ path: 'customers/update/:id', component: UpdateCustomerComponent },
+	{ path: 'customers/delete/:id', component: DeleteCustomerComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+	imports: [RouterModule.forRoot(routes)],
+	exports: [RouterModule]
 })
 export class AppRoutingModule { }
