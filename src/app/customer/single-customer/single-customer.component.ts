ccc10564c1672f52bbff90c460f516497295aa2a
Svein Are Danielsen <sveindani@gmail.com>
2019-03-16 22:10:30 +0100

    Added single view of a customer and orders by them

--- src/app/customer/single-customer/single-customer.component.ts
+++ src/app/customer/single-customer/single-customer.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService } from '../customer.service';
+import { OrderService } from 'src/app/order/order.service';
+import { Customer } from '../customer';
+import { ActivatedRoute } from '@angular/router';
+import { Order } from 'src/app/order/order';
 
 @Component({
-  selector: 'app-single-customer',
-  templateUrl: './single-customer.component.html',
-  styleUrls: ['./single-customer.component.scss']
+	selector: 'app-single-customer',
+	templateUrl: './single-customer.component.html',
+	styleUrls: ['./single-customer.component.scss']
 })
 export class SingleCustomerComponent implements OnInit {
+	constructor(
+		private customerService: CustomerService,
+		private orderService: OrderService,
+		private route: ActivatedRoute,
+	) {}
+	
+	customer: Customer;
 
-  constructor() { }
+	ngOnInit() {
+		let id = this.route.snapshot.params.id
+		this.getCustomer(id);
+		this.getOrders();
+	}
 
-  ngOnInit() {
-  }
+	getCustomer(id) {
+		this.customerService.getCustomer(id).subscribe(customer => this.customer = customer, err => console.error(err), () => {
+			// Finished
+		});
+	}
+
+	getOrders() {
+		this.orderService.getOrders().subscribe(orders => this.customer.orders = orders, err => console.error(err), () => {
+			this.customer.orders.filter(e => {
+				return e.customerId == this.customer.id;
+			});
+		});
+	}
 
 }
