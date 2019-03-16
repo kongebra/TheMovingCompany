efbcad85e08960d21133b7b46eb2c684165852e7
Svein Are Danielsen <sveindani@gmail.com>
2019-03-16 22:09:40 +0100

    Added code for updating orders

--- src/app/order/update-order/update-order.component.ts
+++ src/app/order/update-order/update-order.component.ts
@@ -1,15 +1,60 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../order.service';
+import { CustomerService } from 'src/app/customer/customer.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Order } from '../order';
+import { Customer } from 'src/app/customer/customer';
+import { Eservice } from '../eservice.enum';
 
 @Component({
-  selector: 'app-update-order',
-  templateUrl: './update-order.component.html',
-  styleUrls: ['./update-order.component.scss']
+	selector: 'app-update-order',
+	templateUrl: './update-order.component.html',
+	styleUrls: ['./update-order.component.scss']
 })
 export class UpdateOrderComponent implements OnInit {
+	constructor(
+		private orderService: OrderService,
+		private customerService: CustomerService,
+		private route: ActivatedRoute,
+		private router: Router) { }
 
-  constructor() { }
+	id: string;
+	order: Order;
+	customers: Customer[];
+	services = Eservice;
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.id = this.route.snapshot.paramMap.get('id');
+		this.getOrder(this.id);
+		this.getCustomers();
+	}
+
+	getOrder(id) {
+		this.orderService.getOrder(id).subscribe(order => {
+			this.order = order;
+		}, err => console.error(err), () => {
+			this.getCustomer(this.order.customerId);
+		})
+	}
+
+	putOrder() {
+		this.orderService.putOrder(this.id, this.order).subscribe(() => {
+
+		}, err => console.error(err), () => {
+			this.router.navigate(['/orders']);
+		});
+	}
+
+	getCustomer(id) {
+		this.customerService.getCustomer(id).subscribe(customer => {
+			this.order.customer = customer;
+		});
+	}
+
+	getCustomers() {
+		this.customerService.getCustomers().subscribe(customers => {
+			this.customers = customers;
+		})
+	}
 
 }
