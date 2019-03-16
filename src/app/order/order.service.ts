0023fb96ac2888edcc7c46fce082e7701e41ad02
Svein Are Danielsen <sveindani@gmail.com>
2019-03-16 14:15:55 +0100

    Minor update

--- src/app/order/order.service.ts
+++ src/app/order/order.service.ts
@@ -4,32 +4,32 @@ import { Observable } from 'rxjs';
 import { Order } from './order';
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 
 export class OrderService {
-  // TODO (Svein): Edit this to /api/orders before production
-  url = 'https://localhost:5001/api/orders';
+	// TODO (Svein): Edit this to /api/orders before production
+	url = 'https://localhost:5001/api/orders';
 
-  constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient) { }
 
-  getOrders(): Observable<Order[]> {
-    return this.http.get<Order[]>(this.url);
-  }
+	getOrders(): Observable<Order[]> {
+		return this.http.get<Order[]>(this.url);
+	}
 
-  getOrder(id): Observable<Order> {
-    return this.http.get<Order>(this.url + '/' + id);
-  }
+	getOrder(id): Observable<Order> {
+		return this.http.get<Order>(this.url + '/' + id);
+	}
 
-  postOrder(order: Order): Observable<any> {
-    return this.http.post(this.url, order);
-  }
+	postOrder(order: Order): Observable<any> {
+		return this.http.post(this.url, order);
+	}
 
-  putOrder(id, order: Order): Observable<any> {
-    return this.http.put(this.url + '/' + id, order);
-  }
+	putOrder(id, order: Order): Observable<any> {
+		return this.http.put(this.url + '/' + id, order);
+	}
 
-  deleteOrder(id): Observable<any> {
-    return this.http.delete(this.url + '/' + id);
-  }
+	deleteOrder(id): Observable<any> {
+		return this.http.delete(this.url + '/' + id);
+	}
 }
