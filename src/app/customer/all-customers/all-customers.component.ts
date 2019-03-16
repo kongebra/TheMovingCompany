24462c16a8dcf4ff087e916302acb4a09852a3e9
Svein Are Danielsen <sveindani@gmail.com>
2019-03-16 20:56:50 +0100

    Added function for retrieveing all customers

--- src/app/customer/all-customers/all-customers.component.ts
+++ src/app/customer/all-customers/all-customers.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService } from '../customer.service';
+import { Customer } from '../customer';
 
 @Component({
-  selector: 'app-all-customers',
-  templateUrl: './all-customers.component.html',
-  styleUrls: ['./all-customers.component.scss']
+	selector: 'app-all-customers',
+	templateUrl: './all-customers.component.html',
+	styleUrls: ['./all-customers.component.scss']
 })
 export class AllCustomersComponent implements OnInit {
+	constructor(private customerService: CustomerService) { }
 
-  constructor() { }
+	customers: Customer[] = [];
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.getCustomers();
+	}
+
+	getCustomers() {
+		this.customerService.getCustomers().subscribe(customers => this.customers = customers);
+	}
 
 }
