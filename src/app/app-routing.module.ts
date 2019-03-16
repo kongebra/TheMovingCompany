179ba7393d65069e2c6aa0213f6df8e2d9242654
Svein Are Danielsen <sveindani@gmail.com>
2019-03-16 14:14:45 +0100

    Added some routes for the application

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AllComponent } from './order/all/all.component';
+import { SingleComponent } from './order/single/single.component';
+import { NewComponent } from './order/new/new.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/orders',
+    pathMatch: 'full'
+  },
+  {
+    path: 'order/:id',
+    component: SingleComponent
+  },
+  {
+    path: 'orders',
+    component: AllComponent
+  },
+  {
+    path: 'orders/new',
+    component: NewComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
