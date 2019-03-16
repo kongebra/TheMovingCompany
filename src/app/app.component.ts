335291bfd5973588ffaa58dda77486511f862eca
Svein Are Danielsen <sveindani@gmail.com>
2019-03-16 02:00:20 +0100

    Modified output folder for Angular Added built Angular app into wwwroot Added some testing code in Angular for fetching data from the API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'the-moving-company';
+	constructor(private _httpService: Http) {}
+
+	apiValues: string[] = [];
+	ngOnInit() {
+		this._httpService.get('/api/orders').subscribe(values => {
+			console.log(values);
+			this.apiValues = values.json() as string[];
+		})
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
