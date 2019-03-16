39014bdb54ea125bd2edbc4f3d0fb5d04940c7f9
Svein Are Danielsen <sveindani@gmail.com>
2019-03-16 16:34:18 +0100

    Added one-to-many relationship

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+
 namespace TheMovingCompany.Models {
     public class Customer {
         public long Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
     }
 }
--- Models/Order.cs
+++ Models/Order.cs
@@ -1,14 +1,26 @@
 using System;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace TheMovingCompany.Models {
     public class Order {
         public long Id { get; set; }
+        
+        [Required]
         public string AddressFrom { get; set; }
+        
+        [Required]
         public string AddressTo { get; set; }
+        
+        [Required]
+        [DataType(DataType.Date)]
         public DateTime DueDate { get; set; }
+
         public DateTime Created { get; set; }
+
+        [Required]
         public EService Service { get; set; }
+
         public string Note { get; set; }
 
         // Foreign key for Customer
